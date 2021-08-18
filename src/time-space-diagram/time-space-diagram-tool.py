0064c5c8d7166eed2cf3cb87941f3d5e7ab8070d
debipe20 <debashisdas@email.arizona.edu>
2021-08-18 10:10:09 -0700

    updated the time-space diagram tool

---
+++ src/time-space-diagram/time-space-diagram-tool.py
@@ -0,0 +1,41 @@
+import json 
+from SpatDataManager import SpatDataManager
+from BsmDataManager import BsmDataManager
+from TimeSpaceDiagramManager import TimeSpaceDiagramManager
+
+
+def main():   
+    # Read the config file into a json object:
+    configFile = open("configuration.json", 'r')
+    config = (json.load(configFile))
+    # Close the config file:
+    configFile.close()
+
+    xAxisRange = config["EndTimeOfDiagram"] - config["StartTimeOfDiagram"]
+
+
+    spatDataManager = SpatDataManager(config)
+    bsmDataManager = BsmDataManager(config)
+    timeSpaceDiagramManager = TimeSpaceDiagramManager()
+
+    """
+    Process SPaT file
+    """ 
+    greenTimeStartPoint, clearanceTimeStartPoint, greenTime, clearanceTime, intersectionDistance_Green, intersectionDistance_Clearance = spatDataManager.manageSpatData()
+
+    """
+    Process BSM file
+    """
+    
+    evTrajectoryTimePoint, evTrajectoryDistancePoint, transitTrajectoryTimePoint, transitTrajectoryDistancePoint, truckTrajectoryTimePoint, truckTrajectoryDistancePoint, carTrajectoryTimePoint, carTrajectoryDistancePoint, connectedVehicleTrajectoryTimePoint, connectedVehicleTrajectoryDistancePoint = bsmDataManager.manageBsmData()
+
+    """
+    Plot Time-Space Diagram
+    """
+    timeSpaceDiagramManager.getParameters(greenTimeStartPoint, clearanceTimeStartPoint,
+                     greenTime, clearanceTime, intersectionDistance_Green, intersectionDistance_Clearance, evTrajectoryTimePoint, evTrajectoryDistancePoint, transitTrajectoryTimePoint, transitTrajectoryDistancePoint, truckTrajectoryTimePoint, truckTrajectoryDistancePoint, carTrajectoryTimePoint, carTrajectoryDistancePoint, connectedVehicleTrajectoryTimePoint, connectedVehicleTrajectoryDistancePoint, "Time-Space Diagram", xAxisRange)
+
+    timeSpaceDiagramManager.timeSpaceDiagramMethod()
+    
+if __name__ == "__main__":
+    main()
