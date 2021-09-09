59433dff18f28c06ec522ecc5e047f9370c8c89e
debipe20 <debashisdas@email.arizona.edu>
2021-09-09 11:30:37 -0700

    cleaned up repo

--- src/predict-stock/readme.md
+++
@@ -1,1 +0,0 @@
-link: https://towardsdatascience.com/lstm-time-series-forecasting-predicting-stock-prices-using-an-lstm-model-6223e9644a2f
--- src/vehicle-status-prediction-data-collector/data/data-merge.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import glob
-#merging csv files into txt file. Since excel has 1,048,576 rows, we are using txt file.
-extension = 'csv'
-all_filenames = [j for j in glob.glob('*.{}'.format(extension))]
-combined_csv = pd.concat([pd.read_csv(f, header=None, low_memory=False) for f in all_filenames],axis=0)
-np.savetxt("combined-data-withoutheader.txt", combined_csv.values, fmt='%1.7s', delimiter =",")
